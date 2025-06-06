cf0c3d3439350ec4ad17a9cc52381d751dd4c835
UNICODE-0 <kirill.razvalyaev@gmail.com>
2025-06-06 13:25:30 +0300

    Odin & business cfgs

--- Assets/Code/ECS/Providers/MonoProvider.cs
+++ Assets/Code/ECS/Providers/MonoProvider.cs
@@ -1,11 +1,14 @@
 using Leopotam.EcsLite;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace BusinessClicker.Mono.Providers
 {
     public class MonoProvider<T> : EntityProvider where T: struct 
     {
-        [SerializeField] private T _serializedData;
+        [HideLabel]
+        [SerializeField]
+        private T _serializedData;
         
         private EcsPool<T> _pool;
         public EcsPool<T> Pool => _pool ??= EcsWorld.Default.GetPool<T>();
---
+++ Assets/Code/SO/BusinessCFG.cs
@@ -0,0 +1,40 @@
+using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace BusinessClicker.SO
+{
+    [CreateAssetMenu(fileName = "BusinessCfg", menuName = "ScriptableObjects/BusinessCfg", order = 1)]
+    public class BusinessCfg : ScriptableObject
+    {
+        [Unit(Units.Second)]
+        [MinValue(1)]
+        public int IncomeFrequency = 3;
+        
+        [MinValue(1)]
+        public int BasicCost = 3;
+        
+        [MinValue(1)]
+        public int BasicIncome = 3;
+        
+        [BoxGroup("Upgrade 1")]
+        [HideLabel]
+        public BusinessUpgrade Upgrade1;
+        
+        [BoxGroup("Upgrade 2")]
+        [HideLabel]
+        public BusinessUpgrade Upgrade2;
+    }
+
+    [Serializable]
+    public class BusinessUpgrade
+    {
+        [MinValue(1)]
+        public int Cost = 50;
+        
+        [Unit(Units.Percent)]
+        [MinValue(1)]
+        public int IncomeMultiplier = 50;
+
+    }
+}
